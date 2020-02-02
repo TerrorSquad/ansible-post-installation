678481b8e5b57f8d7503331833448f516d102ec6
Goran Ninkovic <warhawk@hotmail.rs>
2020-02-02 20:49:42 +0100

    add theme-by-time.sh script

---
+++ post-installation/defaults/scripts/theme-by-time.sh
@@ -0,0 +1,36 @@
+#!/bin/dash
+# Kill if already running
+if pidof -o %PPID -x "${0##*/}"; then
+    exit 1
+fi
+DAY_THEME="Adapta"
+NIGHT_THEME="Adapta-Nokto"
+DAY_ICONS="Papirus-Adapta"
+NIGHT_ICONS="Papirus-Adapta-Nokto"
+
+# Start loop
+while :; do
+    # What time is it?
+    CURRENT_TIME=$(date +%H%M)
+    if [ -n "$NEXT_TIME" ] && [ "$CURRENT_TIME" -lt "$NEXT_TIME" ]; then
+        sleep 60
+        continue
+    fi
+    echo ${CURRENT_TIME}
+    # Depending on time set THEME_CHOICE & NEXT_TIME
+    if [ "$CURRENT_TIME" -ge 0600 ] && [ "$CURRENT_TIME" -lt 1800 ]; then
+        THEME_CHOICE=${DAY_THEME}
+        ICONS_CHOICE=${DAY_ICONS}
+        NEXT_TIME=1800
+    else
+        THEME_CHOICE=${NIGHT_THEME}
+        ICONS_CHOICE=${NIGHT_ICONS}
+        NEXT_TIME=0600
+    fi
+    # Set the chosen theme
+    gsettings set org.cinnamon.desktop.interface gtk-theme "$THEME_CHOICE"
+    gsettings set org.cinnamon.desktop.interface icon-theme "$ICONS_CHOICE"
+    gsettings set org.cinnamon.theme name "$THEME_CHOICE"
+    # Sleep
+    sleep 60
+done
