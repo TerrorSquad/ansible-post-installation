f287466ff6e96525a4958b5acb51995d6a3d914e
Goran Ninkovic <change@me.com>
2020-04-06 20:27:57 +0200

    Remove redundant antibody plugins
    Add docker completion plugin

--- post-installation/defaults/.zsh_plugins_antibody.pl
+++ post-installation/defaults/.zsh_plugins_antibody.pl
@@ -2,10 +2,10 @@
 # antibody bundle < ~/.zsh_plugins_antibody.pl > ~/.zsh_plugins_antibody.sh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
+robbyrussell/oh-my-zsh path:plugins/colored-man-pages
 robbyrussell/oh-my-zsh path:plugins/command-not-found
 robbyrussell/oh-my-zsh path:plugins/git
 robbyrussell/oh-my-zsh path:plugins/history
-robbyrussell/oh-my-zsh path:plugins/colored-man-pages
 
 # Bundles from ohmyzsh
 ohmyzsh/ohmyzsh path:lib/grep.zsh
@@ -18,14 +18,7 @@ ohmyzsh/ohmyzsh path:plugins/common-aliases
 lukechilds/zsh-nvm
 
 # Additional completion definitions for Zsh.
+greymd/docker-zsh-completion
 zdharma/fast-syntax-highlighting
 zsh-users/zsh-autosuggestions
 zsh-users/zsh-completions
-
-# Other plugins
-alexrochas/zsh-path-environment-explorer
-unixorn/autoupdate-antigen.zshplugin
-zsh-users/zsh-history-substring-search
-
-# Theme powerlevel10k
-romkatv/powerlevel10k
