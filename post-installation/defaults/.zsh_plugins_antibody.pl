bdc5ac66ffbbe96faff3f41a14f14981494a8adc
Goran Ninkovic <change@me.com>
2020-04-08 12:17:50 +0200

    Remove three antibody plugins and add another one

--- post-installation/defaults/.zsh_plugins_antibody.pl
+++ post-installation/defaults/.zsh_plugins_antibody.pl
@@ -2,18 +2,16 @@
 # antibody bundle < ~/.zsh_plugins_antibody.pl > ~/.zsh_plugins_antibody.sh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
-robbyrussell/oh-my-zsh path:plugins/colored-man-pages
 robbyrussell/oh-my-zsh path:plugins/command-not-found
 robbyrussell/oh-my-zsh path:plugins/git
 robbyrussell/oh-my-zsh path:plugins/history
 
 # Bundles from ohmyzsh
+ohmyzsh/ohmyzsh path:lib/directories.zsh
 ohmyzsh/ohmyzsh path:lib/completion.zsh
-ohmyzsh/ohmyzsh path:lib/grep.zsh
 ohmyzsh/ohmyzsh path:lib/history.zsh
 ohmyzsh/ohmyzsh path:lib/key-bindings.zsh
 ohmyzsh/ohmyzsh path:lib/theme-and-appearance.zsh
-ohmyzsh/ohmyzsh path:plugins/common-aliases
 
 # Node Version Manager
 lukechilds/zsh-nvm
