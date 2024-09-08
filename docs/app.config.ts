9b2714c2d0ad17f1ae2e76c6b1583123c8f7ab35
Goran Ninkovic <goran@goranninkovic.com>
2024-09-08 15:45:21 +0200

    refactor(docs): update app.config.ts for Ansible Post Installation

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,9 +1,9 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Ansible Post Installation',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'Griffin',
+    description: 'Automate your post-installation tasks with Ansible',
+    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     // socials: {
     //   twitter: 'nuxt_js',
     //   github: 'nuxt-themes/docus',
@@ -14,10 +14,10 @@ export default defineAppConfig({
     //   }
     // },
     github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      dir: 'docs/content',
+      branch: 'master',
+      repo: 'ansible-post-installation',
+      owner: 'terrorsquad',
       edit: true
     },
     aside: {
