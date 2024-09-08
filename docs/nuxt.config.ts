d29df6e6eeb08e80d246847ccd9ea869eb73a8ed
Goran Ninkovic <goran@goranninkovic.com>
2024-09-08 14:28:09 +0200

    feat: add GitHub Pages deployment workflow and update Nuxt configuration

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
+
+  app: {
+    baseURL: '/ansible-post-installation/',
+    buildAssetsDir: 'assets',
+  },
+
   devtools: { enabled: true },
 
   modules: [
